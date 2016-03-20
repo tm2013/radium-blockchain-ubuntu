9481f2808b69604dc248a51bf3e26d1b9f6f0ee1
JJ12880 <JJ12880@users.noreply.github.com>
2016-03-19 22:56:38 -0400

    Update build_radium.sh

--- build_radium.sh
+++ build_radium.sh
@@ -38,12 +38,12 @@ make -f makefile.unix USE_UPNP=-
 #################################################################
 
 sudo printf '%s\n%s\n' '#!/bin/sh' '/usr/bin/Radiumd --rpc-endpoint=127.0.0.1:8090 -d /usr/local/Radium/programs/radiumd/'>> /etc/init.d/radium
-
-sudo printf 'rpcuser=%s\n' $2  >> /home/ubuntu/.Radium/Radium.conf
-sudo printf 'rpcpassword=%s\n' $3 >> /home/ubuntu/.Radium/Radium.conf
-sudo printf 'rpcport=%s\n' $5 >> /home/ubuntu/.Radium/Radium.conf
-sudo printf 'rpcallowip=%s\n' $4 >> /home/ubuntu/.Radium/Radium.conf
-sudo printf 'server=1' >> /home/ubuntu/.Radium/Radium.conf
+sudo mkdir ~/.Radium
+sudo printf 'rpcuser=%s\n' $2  >> ~/.Radium/Radium.conf
+sudo printf 'rpcpassword=%s\n' $3 >> ~/.Radium/Radium.conf
+sudo printf 'rpcport=%s\n' $5 >> ~/.Radium/Radium.conf
+sudo printf 'rpcallowip=%s\n' $4 >> ~/.Radium/Radium.conf
+sudo printf 'server=1' >> ~/.Radium/Radium.conf
 
 
 chmod +x /etc/init.d/radium
